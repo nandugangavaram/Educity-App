c569bd61be125f61bfbdfc5cec238277aae2e9db
nandugangavaram <nandugangavaram@gmail.com>
2024-03-22 21:30:16 +1100

    Added navbar and other configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
+      <Navbar />
       <h1>This is App Component</h1>
     </>
   );
